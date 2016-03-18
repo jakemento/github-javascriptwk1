1f1222d1f644b12ea2cc585e3da802c713c064fd
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-18 11:09:21 -0700

    add boldness to index

--- js/github.js
+++ js/github.js
@@ -4,7 +4,7 @@ exports.getRepos = function() {
   $.get('https://api.github.com/users/'+ username + '/repos?access_token=' + apiKey).then(function(response) {
     console.log(response);
     for (i = 0; i < response.length; i++) {
-      $('.showName').append("<li>repo name:" + response[i].name + "</li>");
+      $('.showName').append("<li><strong>repo name:</strong>" + response[i].name + "</li>");
       $('.showName').append("<li>description:" + response[i].description + "</li>");
       $('.showName').append("<li>date:" + response[i].created_at + "</li><br>");
 
