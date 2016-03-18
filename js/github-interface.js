480657365dc4677366872addaa7e34905291d481
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-18 11:07:20 -0700

    sepreate interface jquery from api call js

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,20 +1,15 @@
 var apiKey = require('./../.env').apiKey;
-var username;
+var getRepos = require('./../js/github.js').getRepos;
+
+
+
+// var username;
 
 $(document).ready(function() {
   $('#getUsername').click(function() {
     username = $('#username').val();
     $('#username').val("");
-    $.get('https://api.github.com/users/'+ username + '/repos?access_token=' + apiKey).then(function(response) {
-      console.log(response);
-      for (i = 0; i < response.length; i++) {
-        $('.showName').append("<li>repo name:" + response[i].name + "</li>");
-        $('.showName').append("<li>description:" + response[i].description + "</li>");
-        $('.showName').append("<li>date:" + response[i].created_at + "</li><br>");
-
-      }
-    }).fail(function(error) {
-      $('.showName').text(error.message);
-    });
+    getRepos();
+  
   });
 });
---
+++ js/github.js
@@ -0,0 +1,15 @@
+var apiKey = require('./../.env').apiKey;
+
+exports.getRepos = function() {
+  $.get('https://api.github.com/users/'+ username + '/repos?access_token=' + apiKey).then(function(response) {
+    console.log(response);
+    for (i = 0; i < response.length; i++) {
+      $('.showName').append("<li>repo name:" + response[i].name + "</li>");
+      $('.showName').append("<li>description:" + response[i].description + "</li>");
+      $('.showName').append("<li>date:" + response[i].created_at + "</li><br>");
+
+    }
+  }).fail(function(error) {
+    $('.showName').text(error.message);
+  });
+};
