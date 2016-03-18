86b9f747eff02a9db7e2b93dc2bbfd3ef34906de
Jake Steinberg <jakesteinberg@gmail.com>
2016-03-18 09:31:04 -0700

    update index.html to reflect form submission, add jquery to github-interface which allows for searching api for username

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,1 +1,18 @@
-//JQUERY logic goes here
+var apiKey = require('./../.env').apiKey;
+var username;
+
+$(document).ready(function() {
+  $('#getUsername').click(function() {
+    username = $('#username').val();
+    $('#username').val("");
+    $.get('https://api.github.com/users/'+ username + '/repos?access_token=' + apiKey).then(function(response) {
+      console.log(response);
+      for (i = 0; i < response.length; i++) {
+        $('.showName').append("<li>repo name:" + response[i].name + "</li>");
+        $('.showName').append("<li>description:" + response[i].description + "</li>");
+      }
+    }).fail(function(error) {
+      $('.showName').text(error.message);
+    });
+  });
+});
